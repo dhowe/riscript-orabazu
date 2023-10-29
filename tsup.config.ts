3fe26c9299dc406b9f7271b474ca35161aff1885
dhowe <dhowe@cims.nyu.edu>
2023-10-29 20:57:11 +0800

    sourcemaps

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,17 +3,18 @@ import type { Options } from 'tsup';
 const env = process.env.NODE_ENV;
 
 export const tsup: Options = {
-  splitting: false,
+  name: "riscript",
+  target: 'es2020',
+  entry: ['src/riscript.js'],
+  format: ['cjs', 'esm'], // generate cjs and esm files
+  splitting: true,
   sourcemap: env === 'prod', // source map is only available in prod
   clean: true, // rimraf dist
   dts: false, // generate dts file for main module
-  format: ['cjs', 'esm'], // generate cjs and esm files
-  minify: env === 'production',
-  bundle: env === 'production',
+  minify: env === 'prod',
+  bundle: env === 'prod',
   skipNodeModulesBundle: true,
-  entryPoints: ['src/index.js'],
-  watch: false,//seenv === 'development',
-  target: 'es2020',
-  outDir: env === 'production' ? 'dist' : 'lib',
-  entry: ['src/**/*.js'],
+  watch: false,//env === 'development',
+  outDir: env === 'prod' ? 'dist' : 'lib',
+
 };
